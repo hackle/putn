86f890e3ebee6cc653fc92226dff7044424232b4
Hackle Wayne <hackle.wayne@trademe.co.nz>
2019-05-31 11:15:01 +1200

    calc percentage

--- ItemSalePrice.cs
+++ ItemSalePrice.cs
@@ -12,7 +12,7 @@ namespace Putn
             var discountToApply = Math.Max(memberDiscountPercentage, promoDiscountPercentage);
             
             if (item.IsDiscountable)
-                return item.Price * (1.0m - discountToApply / 100);
+                return item.Price * (100 - discountToApply) / 100;
             else 
                 return item.Price;
         }
