d35077808fbe657305b8a889c55eea25befcb38b
Hackle Wayne <hacklew@gmail.com>
2019-06-05 14:54:19 +1200

    remove purchase and repos

--- IPurchaseRepository.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-
-namespace Putn
-{
-    public interface IPurchaseRepository
-    {
-        void Save(IEnumerable<Purchase> purchases);
-    }
-}
--- Purchase.cs
+++
@@ -1,9 +0,0 @@
-namespace Putn
-{
-    public class Purchase
-    {
-        public int ItemID { get; set; }
-        public decimal PurchasePrice { get; set; }
-        public int MemberID { get; set; }
-    }
-}
