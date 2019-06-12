a2a933dde5ad5f08ca91dc5010167f76213b5902
Hackle Wayne <hacklew@gmail.com>
2019-06-12 20:24:54 +1200

    swap expected and actual

--- ShoppingServiceTests.cs
+++ ShoppingServiceTests.cs
@@ -1,30 +1,67 @@
+using System;
+using System.Linq;
 using Xunit;
 
 namespace Putn
 {
     public class ShoppingServiceTests
     {
-        public static object[][] CalculateTotalPayableTestCases = new object[][] 
+        public static object[][] CalculateTotalPayableTestCases = 
+        new object[][]
         {
             // no item at all
-            new object[] { new Item[]{}, 20, 0 },
-            // single discountable
             new object[] 
-            { 
-                new Item[]{ new Item { IsDiscountable = true, Price = 100 } }, 
-                20, 
-                80
-            },
+            {(
+                items: new Item[]{}, 
+                discountToApply: 20m, 
+                expectedTotal: 0m
+            )},
+            // single discountable
+            new object[]
+            {(
+                items: new Item[]{ new Item { IsDiscountable = true, Price = 100 } }, 
+                discountToApply: 20m, 
+                expectedTotal: 80m
+            )}
             // add more test cases?
         };
 
         [Theory]
         [MemberData(nameof(CalculateTotalPayableTestCases))]
-        public void CalculateTotalPayableTests(Item[] items, decimal discountToApply, decimal expectedTotal)
+        public void CalculateTotalPayableTests_WithTuple(
+            (Item[] items, decimal discountToApply, decimal expectedTotal) testcase)
         {
-            var actual = ShoppingService.CalculateTotalPayable(items, discountToApply);
+            var actual = ShoppingService.CalculateTotalPayable(testcase.items, testcase.discountToApply);
 
-            Assert.Equal(actual, expectedTotal);
+            Assert.Equal(testcase.expectedTotal, actual);
+        }
+
+
+        [Fact]
+        public void CalculateTotalPayableTests_Nested()
+        {
+            var testcases = new []
+            {
+                new  
+                {
+                    items = new Item[]{}, 
+                    discountToApply = 20m, 
+                    expectedTotal = 0m
+                },
+                new 
+                {
+                    items = new Item[]{ new Item { IsDiscountable = true, Price = 100 } }, 
+                    discountToApply = 20m, 
+                    expectedTotal = 80m
+                }
+            };
+
+            foreach (var testcase in testcases)
+            {
+                var actual = ShoppingService.CalculateTotalPayable(testcase.items, testcase.discountToApply);
+
+                Assert.Equal(testcase.expectedTotal, actual);
+            }
         }
     }
 }
