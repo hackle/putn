b624248664e60c24f5eae450213d51f7210a4d5c
Hackle Wayne <hacklew@gmail.com>
2019-06-13 22:39:11 +1200

    normalise cases

--- ShoppingServiceTests.cs
+++ ShoppingServiceTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -44,24 +45,68 @@ namespace Putn
             {
                 new  
                 {
-                    items = new Item[]{}, 
-                    discountToApply = 20m, 
-                    expectedTotal = 0m
+                    Description = "No items",
+                    Items = new Item[]{}, 
+                    DiscountToApply = 20m, 
+                    ExpectedTotal = 0m
                 },
                 new 
                 {
-                    items = new Item[]{ new Item { IsDiscountable = true, Price = 100 } }, 
-                    discountToApply = 20m, 
-                    expectedTotal = 80m
+                    Description = "Discountable item and 20% discount",
+                    Items = new Item[]{ new Item { IsDiscountable = true, Price = 100 } }, 
+                    DiscountToApply = 20m, 
+                    ExpectedTotal = 80m
                 }
             };
 
             foreach (var testcase in testcases)
             {
-                var actual = ShoppingService.CalculateTotalPayable(testcase.items, testcase.discountToApply);
+                var actual = ShoppingService.CalculateTotalPayable(testcase.Items, testcase.DiscountToApply);
 
-                Assert.Equal(testcase.expectedTotal, actual);
+                Assert.Equal(testcase.ExpectedTotal, actual);
             }
         }
+
+        public class CalculateTotalTestCase
+        {
+            public string Description { get; set; }
+            public IEnumerable<Item> Items { get; set; }
+            public decimal DiscountToApply { get; set; }
+            public decimal ExpectedTotal { get; set; }
+        }
+
+        [Theory]
+        [MemberData(nameof(CalculateTotalTestCases))]
+        public void CalculateTotalPayableTests_WithType(CalculateTotalTestCase testcase)
+        {
+            var actual = ShoppingService.CalculateTotalPayable(testcase.Items, testcase.DiscountToApply);
+
+            Assert.Equal(testcase.ExpectedTotal, actual);
+        }
+
+        public static object[][] CalculateTotalTestCases =
+            new object[][]
+            {
+                new object[]
+                {
+                    new CalculateTotalTestCase
+                    {
+                        Description = "No items",
+                        Items = new Item[]{},
+                        DiscountToApply = 20,
+                        ExpectedTotal = 0
+                    },
+                },
+                new object[]
+                {
+                    new CalculateTotalTestCase
+                    {
+                        Description = "Discountable item and 20% discount",
+                        Items = new Item[]{ new Item { IsDiscountable = true, Price = 100 } }, 
+                        DiscountToApply = 20m, 
+                        ExpectedTotal = 80m
+                    },
+                },
+            };
     }
 }
